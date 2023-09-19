1cdbc78a1005fbba82ce71410a205121c82aef94
Nathanael Afangbedjee <nathanael.afangbedjee@student.vinci.be>
2023-09-19 14:56:29 +0200

    1.1 API : read

---
+++ basic/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var usersRouter = require('./routes/users');
+var pizzaRouter = require('./routes/pizzas.js');
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/pizzas', pizzaRouter);
+
+
+module.exports = app;
